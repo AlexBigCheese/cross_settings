56b57ed79828501b74a0c60aa0f40d9430794ae5
dwaindibbly <ajclarke903@gmail.com>
2019-10-05 20:54:34 +0100

    did some things to prepare for publishing

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ![Pub](https://img.shields.io/pub/v/cross_settings)
 
 Cross platform settings, that is all this is.
+If you want to load a settings file, all one needs to do is
+
+```dart
+Settings(vmBase:"appName").loadSettings("settingsFile");
+```
+
+`vmBase` is only used when ran on non-android/non-ios devices, for places like `~/.config/appName`
--- example/main.dart
+++ example/main.dart
@@ -2,7 +2,7 @@ import 'package:cross_settings/cross_settings.dart';
 
 main() async {
   Settings settingsManager = Settings(vmBase: "cross_settings_example");
-  Map settings = await settingsManager.loadSettings("main");
+  Map settings = await settingsManager.loadSettings("main") ?? {};
   var now = DateTime.now();
   print("Currently it's $now");
   var lastRan = DateTime.tryParse(settings["lastOpened"]);
